616e59c8741e7a46b4878a730cb0c25dd4ff8ff5
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-01 14:39:45 +0100

    Implement form layout in FormAddFriend component
    Replaces placeholder content with a form for adding a friend, including fields for name and image URL, and uses the common Button component.

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import Button from './common/Button';
+
 function FormAddFriend() {
-  return <div>FormAddFriend</div>;
+  return (
+    <form action="" className="form-add-friend">
+      <label htmlFor="new-friend-name">🤼‍♀️ Friend name</label>
+      <input type="text" placeholder="Tiffany" id="new-friend-name" required />
+
+      <label htmlFor="new-friend-image">📷 Image URL</label>
+      <input type="url" placeholder="https://..." id="new-friend-image" required />
+
+      <Button>Add</Button>
+    </form>
+  );
 }
 
 export default FormAddFriend;
