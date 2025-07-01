e225b9c8c6b17a33671616751b1c4098672cc674
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-01 14:21:02 +0100

    Add FormAddFriend component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import './App.css';
 import InitialFriends from './data/users.json';
 
 import FriendsList from './components/FriendsList';
+import FormAddFriend from './components/FormAddFriend';
 
 const friends = InitialFriends;
-// console.log(friends);
 
 function App() {
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList friends={friends} />
+        <FormAddFriend />
       </div>
     </div>
   );
---
+++ src/components/FormAddFriend.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FormAddFriend() {
+  return <div>FormAddFriend</div>;
+}
+
+export default FormAddFriend;
