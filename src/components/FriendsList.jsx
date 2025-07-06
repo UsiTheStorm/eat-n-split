880ada5ec5aa48666753902ea627e34fa100120f
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-07-06 13:15:26 +0100

    Cleanup

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -9,9 +9,10 @@ function Friend({ friend: { name, image, balance } }) {
 
   const handleImageError = () => setImageError(true);
 
+  //  adding placeholder image in case of error
   const displayedImage =
     imageError || !image
-      ? 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png' // URL вашого плейсхолдера
+      ? 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png'
       : image;
 
   if (balance < 0) {
